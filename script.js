9e60f2879c8286b4a2fe58ce896ee14ca6431818
Gonzalop0210 <gonzalopalmadev0210@gmail.com>
2023-10-27 10:27:43 -0500

    añadiendo archivos

---
+++ script.js
@@ -0,0 +1,10 @@
+let back = document.body
+
+let cambioFondo = ()=>{
+  if (back.style.backgroundColor === 'royalblue') {
+    back.style.backgroundColor = 'tomato'
+  } else {
+    back.style.backgroundColor = 'royalblue'
+  }
+}
+
